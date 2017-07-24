fa1688f61cd77538ecc527a5c7e7f55628565adc
Florent Castelli <florent.castelli@gmail.com>
2017-07-24 23:35:57 +0200

    Add table models

--- js/script.js
+++ js/script.js
@@ -1,43 +1,62 @@
 Set.prototype.difference = function(setB) {
-    var difference = new Set(this);
-    for (var elem of setB) {
+    let difference = new Set(this);
+    for (let elem of setB) {
         difference.delete(elem);
     }
     return difference;
 }
 
 $( function() {
-  var locations = [{
-    "door": "Link's house",
-    "cave": "Link's house",
-    "exit": "Link's house exit"
-  }];
+  let locations = [];
 
-  var randomizer_mode = "normal";
+  let randomizer_mode = "normal";
 
-  var door_locations = [
-    "none",
-    "Forest chest game",
-    "Thief's hideout drop",
-    "Thief's hideout door",
-    "Lumberjack cave drop",
-    "Lumberjack cave door",
-    "Lumberjack house",
-    "Death Mountain Door front",
-    "Death Mountain Door back",
-    "Death Mountain Exit front",
-    "Death Mountain Exit back",
-  ];
+  let door_locations = {
+    "Forest chest game": {
+      "region": "Lost woods",
+    },
+    "Thief's hideout drop": {
+      "region": "Lost woods",
+    },
+    "Thief's hideout door": {
+      "region": "Lost woods",
+    },
+    "Lumberjack cave drop": {
+      "region": "Overworld North",
+    },
+    "Lumberjack cave door": {
+      "region": "Overworld North",
+    },
+    "Lumberjack house": {
+      "region": "Overworld North",
+    },
+    "Death Mountain Door front": {
+      "region": "Overworld North",
+    },
+    "Death Mountain Door back": {
+      "region": "Overworld North",
+    },
+    "Death Mountain Exit front": {
+      "region": "Overworld North",
+    },
+    "Death Mountain Exit back": {
+      "region": "Overworld North",
+    },
+    "Link's house": {
+      "region": "Overworld Center",
+    },
+  };
 
-  var caves = [
+  let caves = [
+    "Useless",
     "Link's house",
-    "None",
     "Thief's hideout",
-    "Death mountain door"
+    "Death mountain climbing cave",
+    "Lumberjack cave",
   ]
 
   $("#add_location_text_door").autocomplete({
-    source: door_locations,
+    source: [],
     minLength: 0,
   });
 
@@ -47,43 +66,97 @@ $( function() {
   });
 
   $( "#add_location_form" ).submit(function(event) {
-    var text_door = $("#add_location_text_door").val();
-    var text_cave = $("#add_location_text_cave").val();
+    let text_door = $("#add_location_text_door").val();
+    let text_cave = $("#add_location_text_cave").val();
     locations.push({
       "door": text_door,
       "cave": text_cave,
-      "exit": "Foobar",
+      "exit": text_door,
     });
     refreshList();
+    $("#add_location_text_door").val("");
+    $("#add_location_text_cave").val("");
+    $("#add_location_text_door").focus();
     event.preventDefault();
   });
 
+  $('#locations_table').DataTable({
+    paging: false,
+    info: false,
+    columns: [
+      { "data": "door"},
+      { "data": "cave"},
+      { "data": "exit"},
+    ],
+  });
+  $('#unvisited_doors_table').DataTable({
+    paging: false,
+    info: false,
+    columns: [
+      { "data": "name"},
+      { "data": "region"},
+    ],
+  });
+  $('#unvisited_caves_table').DataTable({
+    paging: false,
+    info: false,
+  });
+
   function refreshList() {
-    var location_table = $("#location_table").children("tbody");
-    var unvisited_doors_table = $("#unvisited_doors_table").children("tbody");
-    location_table.empty();
-    unvisited_doors_table.empty();
+    let locations_table = $("#locations_table").dataTable().api();
+    let unvisited_doors_table = $("#unvisited_doors_table").dataTable().api();
+    let unvisited_caves_table = $("#unvisited_caves_table").dataTable().api();
 
-    var all_doors = new Set(door_locations);
-    all_doors.delete("none");
-    var found_door = new Set();
+    let all_doors = new Set();
+    for(let name of Object.keys(door_locations)) {
+      all_doors.add(name);
+    }
+    let found_door = new Set();
 
+    let all_caves = new Set(caves);
+    let found_caves = new Set();
+    all_caves.delete(caves[0]);
 
-    locations.forEach(function(item) {
+    let locations_array = [];
+    for(let item of locations) {
       found_door.add(item.door);
+      found_caves.add(item.cave);
+      locations_array.push({
+        "door": item.door,
+        "cave": item.cave,
+        "exit": item.exit,
+      });
+    }
+    locations_table.clear();
+    locations_table.rows.add(locations_array);
+    locations_table.rows().invalidate().draw();
 
-      location_table.append($('<tr>')
-        .append($('<td>').text(item.door))
-        .append($('<td>').text(item.cave))
-        .append($('<td>').text(item.exit))
-      );
-    });
+    // Update the unvisited doors model
+    let unvisited_doors = all_doors.difference(found_door);
+    let unvisited_doors_array = [];
+    for(let item of unvisited_doors) {
+      unvisited_doors_array.push({
+        "name": item,
+        "region": door_locations[item].region,
+      });
+    }
+    unvisited_doors_table.clear();
+    unvisited_doors_table.rows.add(unvisited_doors_array);
+    unvisited_doors_table.rows().invalidate().draw();
+    $("#add_location_text_door").autocomplete("option", "source", Array.from(unvisited_doors));
 
-    var unvisited_doors = all_doors.difference(found_door);
-    unvisited_doors.forEach(function(item) {
-      unvisited_doors_table.append($('<tr>')
-        .append($('<td>').text(item))
-      );
-    });
+    // Update the unvisited caves model
+    let unvisited_caves = all_caves.difference(found_caves);
+    let unvisited_caves_array = [];
+    for(let item of unvisited_caves) {
+      unvisited_caves_array.push([item]);
+    }
+    unvisited_caves_table.clear();
+    unvisited_caves_table.rows.add(unvisited_caves_array);
+    unvisited_caves_table.rows().invalidate().draw();
+    //$("#add_location_text_cave").autocomplete("option", "source", Array.from(unvisited_caves));
+    $("#add_location_text_cave").autocomplete("option", "source", Array.from(caves));
   }
+
+  refreshList();
 });

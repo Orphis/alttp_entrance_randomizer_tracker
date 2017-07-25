b40f0d6c3b475e77a2ac8863ac9c239936ac8218
Florent Castelli <florent.castelli@gmail.com>
2017-07-25 02:40:16 +0200

    Update doors list

--- js/doors.js
+++ js/doors.js
@@ -1,36 +1,421 @@
 window.door_locations = {
-    "Forest chest game": {
-      "region": "Lost woods",
-    },
-    "Thief's hideout drop": {
-      "region": "Lost woods",
-    },
-    "Thief's hideout door": {
-      "region": "Lost woods",
-    },
-    "Lumberjack cave drop": {
-      "region": "Overworld North",
-    },
-    "Lumberjack cave door": {
-      "region": "Overworld North",
-    },
-    "Lumberjack house": {
-      "region": "Overworld North",
-    },
-    "Death Mountain Door front": {
-      "region": "Overworld North",
-    },
-    "Death Mountain Door back": {
-      "region": "Overworld North",
-    },
-    "Death Mountain Exit front": {
-      "region": "Overworld North",
-    },
-    "Death Mountain Exit back": {
-      "region": "Overworld North",
-    },
-    "Link's house": {
-      "region": "Overworld Center",
-    },
-  //};
+  //Light World
+  "Forest Chest Game": {
+    "region": "Lost Woods",
+  },
+  "Thief's Hideout Drop": {
+    "region": "Lost Woods",
+  },
+  "Thief's Hideout Door": {
+    "region": "Lost Woods",
+  },
+  "Lumberjack Cave Drop": {
+    "region": "Light World North",
+  },
+  "Lumberjack Cave Door": {
+    "region": "Light World North",
+  },
+  "Lumberjack House": {
+    "region": "Light World North",
+  },
+  "Death Mountain Entrance Front": {
+    "region": "Light World North",
+  },
+  "Death Mountain Exit Back": {
+    "region": "Light World North",
+  },
+  "Sanctuary Bonk Rocks": {
+    "region": "Light World North",
+  },
+  "Sanctuary": {
+    "region": "Light World North",
+  },
+  "Sewer Grave Drop": {
+    "region": "Light World North",
+  },
+  "Graveyard Cliff": {
+    "region": "Light World North",
+  },
+  "King's Tomb": {
+    "region": "Light World North",
+  },
+  "East of Graveyard Drop": { //maybe
+    "region": "Light World North",
+  },
+  "East of Graveyard Door": { //maybe
+    "region": "Light World North",
+  },
+  "Death Mountain Entrance Back": {
+    "region": "Death Mountain",
+  },
+  "Death Mountain Exit Front": {
+    "region": "Death Mountain",
+  },
+  "Old Man's Cave Front": {
+    "region": "Death Mountain",
+  },
+  "Old Man's Cave Back": {
+    "region": "Death Mountain",
+  },
+  "Spectacle Rock Item Peak": {
+    "region": "Death Mountain",
+  },
+  "Spectacle Rock Item Get": {
+    "region": "Death Mountain",
+  },
+  "Spectacle Rock Exit": {
+    "region": "Death Mountain",
+  },
+  "Paradox Cave Top": { //maybe
+    "region": "Death Mountain",
+  },
+  "Paradox Cave Bottom": { //maybe
+    "region": "Death Mountain",
+  },
+  "Mimic Cave": {
+    "region": "Death Mountain",
+  },
+  "Spiral Cave Top": {
+    "region": "Death Mountain",
+  },
+  "Spiral Cave Bottom": {
+    "region": "Death Mountain",
+  },
+  "Light World Bitch Door Front": { //maybe
+    "region": "Death Mountain",
+  },
+  "Light World Bitch Door Exit": { //maybe
+    "region": "Death Mountain",
+  },
+  "Twin Cave Left": {
+    "region": "Death Mountain",
+  },
+  "Twin Cave Right": {
+    "region": "Death Mountain",
+  },
+  "Witch's Hut": {
+    "region": "Light World East",
+  },
+  "Waterfall Fairy": {
+    "region": "Light World East",
+  },
+  "Tree Cave by Eastern Palace": { //definitely
+    "region": "Light World East",
+  },
+  "Cave by Eastern Palace Portal": { //definitely
+    "region": "Light World East",
+  },
+  "Sahasrahla's Hut": {
+    "region": "Light World East",
+  },
+  "Kakariko Fortune Teller North": {
+    "region": "Kakariko",
+  },
+  "Old Woman's House Left": {
+    "region": "Kakariko",
+  },
+  "Old Woman's House Right": {
+    "region": "Kakariko",
+  },
+  "Blind's Hut": {
+    "region": "Kakariko",
+  },
+  "Kakariko Well Drop": {
+    "region": "Kakariko",
+  },
+  "Kakariko Well Door": {
+    "region": "Kakariko",
+  },
+  "House West of Bottle Vendor": { //definitely
+    "region": "Kakariko",
+  },
+  "House East of Bottle Vendor": { //definitely
+    "region": "Kakariko",
+  },
+  "Chicken House": {
+    "region": "Kakariko",
+  },
+  "Sick Kid's House": {
+    "region": "Kakariko",
+  },
+  "House right of sick Kid": { //definitely
+    "region": "Kakariko",
+  },
+  "Tavern Back Entrance": {
+    "region": "Kakariko",
+  },
+  "Tavern Front Entrance": {
+    "region": "Kakariko",
+  },
+  "Shop beside Tavern": { //definitely
+    "region": "Kakariko",
+  },
+  "Doorless Shed": {
+    "region": "Kakariko",
+  },
+  "Smith's House": {
+    "region": "Kakariko",
+  },
+  "Bat Cave Drop": {
+    "region": "Kakariko",
+  },
+  "Bat Cave Door": {
+    "region": "Kakariko",
+  },
+  "Library": {
+    "region": "Kakariko",
+  },
+  "Race Game Right": {
+    "region": "Kakariko",
+  },
+  "Race Game Left": {
+    "region": "Kakariko",
+  },
+  "Kakariko Fortune Teller South": {
+    "region": "Kakariko",
+  },
+  "Lake Hylia Fortune Teller": {
+    "region": "Lake Hylia",
+  },
+  "Cave North of Lake Hylia": { //maybe
+    "region": "Lake Hylia",
+  },
+  "Lake Hylia Fairy Cave": {
+    "region": "Lake Hylia",
+  },
+  "Ice Bomb Cave": {
+    "region": "Lake Hylia",
+  },
+  "Ice Fairy Cave": {
+    "region": "Lake Hylia",
+  },
+  "Ice Rock Cave": {
+    "region": "Lake Hylia",
+  },
+  "Mini Moldorm Cave": {
+    "region": "Lake Hylia",
+  },
+  "South of Grove": {
+    "region": "Light World South",
+  },
+  "Link's House Bonk Rocks": {
+    "region": "Light World South",
+  },
+  "Link's House": {
+    "region": "Light World South",
+  },
+  "Light World Hype Cave Spot": { //maybe
+    "region": "Light World South",
+  },
+  "Dam": {
+    "region": "Light World South",
+  },
+  "Rock Rupee Cave by Desert": { //maybe
+    "region": "Desert Area",
+  },
+  "Regular Cave by Desert": { //maybe
+    "region": "Desert Area",
+  },
+  "Aginah's Cave": {
+    "region": "Desert Area",
+  },
+  "Checkerboard Cave": {
+    "region": "Desert Area",
+  },
+  "Uncle Drop": {
+    "region": "Castle",
+  },
+  "Uncle Door": {
+    "region": "Castle",
+  },
+
+//Dark World
+  "DW Lumberjack House": {
+    "region": "Dark World North",
+  },
+  "Bumper Cave Bottom": {
+    "region": "Dark World North",
+  },
+  "Bumper Cave Top": {
+    "region": "Dark World North",
+  },
+  "DW Sanctuary Spot": {
+    "region": "Dark World North",
+  },
+  "Fire Shield Shop": { //maybe
+    "region": "Dark World North",
+  },
+  "DW Witch's Hut": {
+    "region": "Dark World East",
+  },
+  "Tree Cave by Palace of Darkness": { //maybe
+    "region": "Dark World East",
+  },
+  "DW Cave by Eastern Palace Portal": { //maybe
+    "region": "Dark World East",
+  },
+  "Monkey Hut": {
+    "region": "Dark World East",
+  },
+  "Village of Outcasts Fortune Teller": {
+    "region": "Village of Outcasts",
+  },
+  "Chest Game": {
+    "region": "Village of Outcasts",
+  },
+  "C House": {
+    "region": "Village of Outcasts",
+  },
+  "House South of C House": { //definitely
+    "region": "Village of Outcasts",
+  },
+  "Doorless Hut": {
+    "region": "Village of Outcasts",
+  },
+  "Peg Cave": {
+    "region": "Village of Outcasts",
+  },
+  "Bow Game House": {
+    "region": "Dark World South",
+  },
+  "Bonk Rocks by Bomb Shop": {
+    "region": "Dark World South",
+  },
+  "Bomb Shop": {
+    "region": "Dark World South",
+  },
+  "Hype Cave": {
+    "region": "Dark World South",
+  },
+  "DW Lake Hylia House": {
+    "region": "DW Lake Hylia",
+  },
+  "DW Ice Bomb Cave": {
+    "region": "DW Lake Hylia",
+  },
+  "DW Ice Fairy Cave": {
+    "region": "DW Lake Hylia",
+  },
+  "DW Ice Rock Cave": {
+    "region": "DW Lake Hylia",
+  },
+  "Left Mire Head": {
+    "region": "Mire Area",
+  },
+  "Right Mire Head": {
+    "region": "Mire Area",
+  },
+  "Mire Cave": {
+    "region": "Mire Area",
+  },
+  "Pyramid Fairy": {
+    "region": "Pyramid Area",
+  },
+  "Ganon Drop": {
+    "region": "Pyramid Area",
+  },
+  "Ganon Door": {
+    "region": "Pyramid Area",
+  },
+  "Spike Cave": {
+    "region": "DW Death Mountain",
+  },
+  "West DM Fairy Cave": { //maybe
+    "region": "DW Death Mountain",
+  },
+  "DW Twin Cave Left": {
+    "region": "DW Death Mountain",
+  },
+  "DW Twin Cave Right": {
+    "region": "DW Death Mountain",
+  },
+  "DW East DM Upper Exit": {
+    "region": "DW Death Mountain",
+  },
+  "Hookshot Cave Front": {
+    "region": "DW Death Mountain",
+  },
+  "Hookshot Cave Back": {
+    "region": "DW Death Mountain",
+  },
+
+//Dungeons
+  "Eastern Palace": {
+    "region": "Light World East",
+  },
+  "Desert Main Entrance": {
+    "region": "Desert Area",
+  },
+  "Desert West": {
+    "region": "Desert Area",
+  },
+  "Desert East": {
+    "region": "Desert Area",
+  },
+  "Desert Back": {
+    "region": "Desert Area",
+  },
+  "Castle Main Entrance": {
+    "region": "Castle",
+  },
+  "Castle Left": {
+    "region": "Castle",
+  },
+  "Castle Right": {
+    "region": "Castle",
+  },
+  "Castle Aga": {
+    "region": "Castle",
+  },
+  "Skull Woods Drop Left": {
+    "region": "Skull Woods",
+  },
+  "Skull Woods Drop Right": {
+    "region": "Skull Woods",
+  },
+  "Skull Woods Drop Back": {
+    "region": "Skull Woods",
+  },
+  "Skull Woods Front Entrance": {
+    "region": "Skull Woods",
+  },
+  "Skull Woods Second Entrance": {
+    "region": "Skull Woods",
+  },
+  "Skull Woods Exit to Back": {
+    "region": "Skull Woods",
+  },
+  "Skull Woods Big Skull": {
+    "region": "Skull Woods",
+  },
+  "Thieve's Town": {
+    "region": "Village of Outcasts",
+  },
+  "Palace of Darkness": {
+    "region": "Dark World East",
+  },
+  "Ice Palace": {
+    "region": "DW Lake Hylia",
+  },
+  "Swamp Palace": {
+    "region": "Dark World South",
+  },
+  "Misery Mire": {
+    "region": "Mire Area",
+  },
+  "Trock Medaillion Entrance": {
+    "region": "DW Death Mountain",
+  },
+  "Trock Big Chest Left": {
+    "region": "DW Death Mountain",
+  },
+  "Trock Big Chest Left": {
+    "region": "DW Death Mountain",
+  },
+  "Trock Laser Bridge Door": {
+    "region": "DW Death Mountain",
+  },
+  "Ganon's Tower": {
+    "region": "DW Death Mountain",
+  },
 }

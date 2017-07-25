66d2cadc801ac984237568fb8e8c355a2b484e41
Florent Castelli <florent.castelli@gmail.com>
2017-07-25 15:20:26 +0200

    Update list of caves

--- js/caves.js
+++ js/caves.js
@@ -2,16 +2,124 @@ window.caves = {
   "Useless": {
     "count": 0,
   },
-  "Link's house": {
-    "count": 1,
-  },
-  "Thief's hideout": {
-    "count": 1,
-  },
-  "Death mountain hype cave": {
+  "Death Mountain Hype Cave": {
     "count": 7,
   },
-  "Lumberjack cave": {
+  "Blind's Hut": {
+    "count": 5,
+  },
+  "Mini Moldorm Cave": {
+    "count": 5,
+  },
+  "Hype Cave": {
+    "count": 5,
+  },
+  "Kakariko Well": {
+    "count": 5,
+  },
+  "Hookshot Cave": {
+    "count": 4,
+  },
+  "Sahasrahla's Hut": {
+    "count": 4,
+  },
+  "Sewer Grave": {
+    "count": 3,
+  },
+  "Fat Fairy": {
+    "count": 2,
+  },
+  "West of Mire": {
+    "count": 2,
+  },
+  "Super Bunny Cave": {
+    "count": 2,
+  },
+  "Sanctuary": {
     "count": 1,
+  },
+  "Link's House": {
+    "count": 1,
+  },
+  "Thief's Hideout": {
+    "count": 1,
+  },
+  "Lumberjack Cave": {
+    "count": 1,
+  },
+  "Ice Rod Cave": {
+    "count": 1,
+  },
+  "Checkerboard Cave": {
+    "count": 1,
+  },
+  "Aginah": {
+    "count": 1,
+  },
+  "South of Grove": {
+    "count": 1,
+  },
+  "Chicken House": {
+    "count": 1,
+  },
+  "Back of Tavern": {
+    "count": 1,
+  },
+  "Peg Cave": {
+    "count": 1,
+  },
+  "Bonk Cave": {
+    "count": 1,
+  },
+  "King's Tomb": {
+    "count": 1,
+  },
+  "Spectacle Rock Cave": {
+    "count": 1,
+  },
+  "C House": {
+    "count": 1,
+  },
+  "Doorless Hut": {
+    "count": 1,
+  },
+  "Chest Game": {
+    "count": 1,
+  },
+  "Spike Cave": {
+    "count": 1,
+  },
+  "Spiral Cave": {
+    "count": 1,
+  },
+  "Mimic Cave": {
+    "count": 1,
+  },
+  "Graveyard Cliff": {
+    "count": 1,
+  },
+  "Library": {
+    "count": 1,
+  },
+  "Smiths": {
+    "count": 1,
+  },
+  "Bat Cave": {
+    "count": 1,
+  },
+  "Sick Kid": {
+    "count": 1,
+  },
+  "Old Man": {
+    "count": 1,
+  },
+  "Witch's Hut": {
+    "count": 1,
+  },
+  "Dam": {
+    "count": 1,
+  },
+  "Bomb Shop": {
+    "count": 0,
   },
 };

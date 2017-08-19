f950bfb0bc9106bc6f304c81b59ac4af39ccd372
Florent Castelli <florent.castelli@gmail.com>
2017-08-19 16:17:48 +0200

    Add all LW door locations

--- js/doors.js
+++ js/doors.js
@@ -53,295 +53,431 @@ window.doorLocations = {
   'Sanctuary Bonk Rocks': {
     region: 'Light World North',
     tag: 'lw',
+    x: '38.36%',
+    y: '29.19%',
   },
   Sanctuary: {
     region: 'Light World North',
     tag: 'lw',
+    x: '46.35%',
+    y: '26.70%',
   },
   'Sewer Grave Drop': {
     region: 'Light World North',
     tag: 'lw',
     drop: true,
+    x: '52.74%',
+    y: '28.96%',
   },
   'Graveyard Cliff': {
     region: 'Light World North',
     tag: 'lw',
+    x: '57.53%',
+    y: '27.38%',
   },
   "King's Tomb": {
     region: 'Light World North',
     tag: 'lw',
+    x: '60.27%',
+    y: '28.96%',
   },
   'East of Graveyard Drop': {
     // maybe
     region: 'Light World North',
     drop: true,
     tag: 'lw',
+    x: '64.61%',
+    y: '30.54%',
   },
   'East of Graveyard Door': {
     // maybe
     region: 'Light World North',
     tag: 'lw',
+    x: '67.61%',
+    y: '27.54%',
   },
   'Death Mountain Entrance Back': {
     region: 'Death Mountain',
     tag: 'lw',
+    x: '40.64%',
+    y: '19.23%',
   },
   'Death Mountain Exit Front': {
     region: 'Death Mountain',
     tag: 'lw',
+    x: '39.50%',
+    y: '14.03%',
   },
   "Old Man's Cave Front": {
     region: 'Death Mountain',
     tag: 'lw',
+    x: '45.43%',
+    y: '22.62%',
   },
   "Old Man's Cave Back": {
     region: 'Death Mountain',
     tag: 'lw',
+    x: '53.65%',
+    y: '15.84%',
   },
   'Spectacle Rock Item Peak': {
     region: 'Death Mountain',
     tag: 'lw',
+    x: '48.63%',
+    y: '10.41%',
   },
   'Spectacle Rock Item Get': {
     region: 'Death Mountain',
     tag: 'lw',
+    x: '45.89%',
+    y: '14.25%',
   },
   'Spectacle Rock Exit': {
     region: 'Death Mountain',
     tag: 'lw',
+    x: '48.63%',
+    y: '14.71%',
   },
   'Paradox Cave Top': {
     // maybe
     region: 'Death Mountain',
     tag: 'lw',
+    x: '86.53%',
+    y: '6.11%',
   },
   'Paradox Cave Bottom': {
     // maybe
     region: 'Death Mountain',
     tag: 'lw',
+    x: '86.99%',
+    y: '21.49%',
   },
   'Mimic Cave': {
     region: 'Death Mountain',
     tag: 'lw',
+    x: '84.93%',
+    y: '9.28%',
   },
   'Spiral Cave Top': {
     region: 'Death Mountain',
     tag: 'lw',
+    x: '79.00%',
+    y: '9.28%',
   },
   'Spiral Cave Bottom': {
     region: 'Death Mountain',
     tag: 'lw',
+    x: '79.00%',
+    y: '13.35%',
   },
   'Light World Bitch Door Front': {
     // maybe
     region: 'Death Mountain',
     tag: 'lw',
+    x: '82%',
+    y: '14.71%',
   },
   'Light World Bitch Door Exit': {
     // maybe
     region: 'Death Mountain',
     tag: 'lw',
+    x: '82%',
+    y: '11.31%',
   },
   'Twin Cave Left': {
     region: 'Death Mountain',
     tag: 'lw',
+    x: '85.00%',
+    y: '14.71%',
   },
   'Twin Cave Right': {
     region: 'Death Mountain',
     tag: 'lw',
+    x: '88.00%',
+    y: '14.71%',
   },
   "Witch's Hut": {
     region: 'Light World East',
     tag: 'lw',
+    x: '82.65%',
+    y: '31.90%',
   },
   'Waterfall Fairy': {
     region: 'Light World East',
     tag: 'lw',
+    x: '90.64%',
+    y: '14.25%',
   },
   'Tree Cave by Eastern Palace': {
     // definitely
     region: 'Light World East',
     tag: 'lw',
+    x: '83.11%',
+    y: '64.25%',
   },
   'Cave by Eastern Palace Portal': {
     // definitely
     region: 'Light World East',
     tag: 'lw',
+    x: '98.40%',
+    y: '70.14%',
   },
   "Sahasrahla's Hut": {
     region: 'Light World East',
     tag: 'lw',
+    x: '81.96%',
+    y: '44.80%',
   },
   'Kakariko Fortune Teller North': {
     region: 'Kakariko',
     tag: 'lw',
+    x: '18.72%',
+    y: '31.90%',
   },
   "Old Woman's House Left": {
     region: 'Kakariko',
     tag: 'lw',
+    x: '16.00%',
+    y: '41.63%',
   },
   "Old Woman's House Right": {
     region: 'Kakariko',
     tag: 'lw',
+    x: '19.00%',
+    y: '41.63%',
   },
   "Blind's Hut": {
     region: 'Kakariko',
     tag: 'lw',
+    x: '13.00%',
+    y: '41.63%',
   },
   'Kakariko Well Drop': {
     region: 'Kakariko',
     drop: true,
     tag: 'lw',
+    x: '2.50%',
+    y: '42.53%',
   },
   'Kakariko Well Door': {
     region: 'Kakariko',
     tag: 'lw',
+    x: '5.50%',
+    y: '42.53%',
   },
   'House West of Bottle Vendor': {
     // definitely
     region: 'Kakariko',
     tag: 'lw',
+    x: '5.25%',
+    y: '46.61%',
   },
   'House East of Bottle Vendor': {
     // definitely
     region: 'Kakariko',
     tag: 'lw',
+    x: '21.23%',
+    y: '47.96%',
   },
   'Chicken House': {
     region: 'Kakariko',
     tag: 'lw',
+    x: '9.82%',
+    y: '52.94%',
   },
   "Sick Kid's House": {
     region: 'Kakariko',
     tag: 'lw',
+    x: '15.98%',
+    y: '52.94%',
   },
   'House right of sick Kid': {
     // definitely
     region: 'Kakariko',
     tag: 'lw',
+    x: '20.32%',
+    y: '52.94%',
   },
   'Tavern Back Entrance': {
     region: 'Kakariko',
     tag: 'lw',
+    x: '15.75%',
+    y: '57.00%',
   },
   'Tavern Front Entrance': {
     region: 'Kakariko',
     tag: 'lw',
+    x: '15.75%',
+    y: '60.00%',
   },
   'Shop beside Tavern': {
     // definitely
     region: 'Kakariko',
     tag: 'lw',
+    x: '11.42%',
+    y: '57.24%',
   },
   'Doorless Shed': {
     region: 'Kakariko',
     tag: 'lw',
+    x: '2.28%',
+    y: '58.82%',
   },
   "Smith's House": {
     region: 'Kakariko',
     tag: 'lw',
+    x: '30.37%',
+    y: '52.94%',
   },
   'Bat Cave Drop': {
     region: 'Kakariko',
     drop: true,
     tag: 'lw',
+    x: '34.05%',
+    y: '55.88%',
   },
   'Bat Cave Door': {
     region: 'Kakariko',
     tag: 'lw',
+    x: '31.05%',
+    y: '55.88%',
   },
   Library: {
     region: 'Kakariko',
     tag: 'lw',
+    x: '15.75%',
+    y: '65.38%',
   },
   'Race Game Right': {
     region: 'Kakariko',
     tag: 'lw',
+    x: '14.16%',
+    y: '71.27%',
   },
   'Race Game Left': {
     region: 'Kakariko',
     tag: 'lw',
+    x: '10.50%',
+    y: '71.27%',
   },
   'Kakariko Fortune Teller South': {
     region: 'Kakariko',
     tag: 'lw',
+    x: '21.92%',
+    y: '70.36%',
   },
   'Lake Hylia Fortune Teller': {
     region: 'Lake Hylia',
     tag: 'lw',
+    x: '65.53%',
+    y: '80.09%',
   },
   'Cave North of Lake Hylia': {
     // maybe
     region: 'Lake Hylia',
     tag: 'lw',
+    x: '73.52%',
+    y: '76.47%',
   },
   'Lake Hylia Fairy Cave': {
     region: 'Lake Hylia',
     tag: 'lw',
+    x: '80.37%',
+    y: '85.07%',
   },
   'Ice Bomb Cave': {
     region: 'Lake Hylia',
     tag: 'lw',
+    x: '89.95%',
+    y: '76.47%',
   },
   'Ice Fairy Cave': {
     region: 'Lake Hylia',
     tag: 'lw',
+    x: '92.95%',
+    y: '76.47%',
   },
   'Ice Rock Cave': {
     region: 'Lake Hylia',
     tag: 'lw',
+    x: '91.45%',
+    y: '79.47%',
   },
   'Mini Moldorm Cave': {
     region: 'Lake Hylia',
     tag: 'lw',
+    x: '65.30%',
+    y: '93.67%',
   },
   'South of Grove': {
     region: 'Light World South',
     tag: 'lw',
+    x: '26.48%',
+    y: '82.58%',
   },
   "Link's House Bonk Rocks": {
     region: 'Light World South',
     tag: 'lw',
+    x: '47.72%',
+    y: '65.38%',
   },
   "Link's House": {
     region: 'Light World South',
     tag: 'lw',
+    x: '55.25%',
+    y: '68.78%',
   },
   'Light World Hype Cave Spot': {
     // maybe
     region: 'Light World South',
     tag: 'lw',
+    x: '60.27%',
+    y: '77.60%',
   },
   Dam: {
     region: 'Light World South',
     tag: 'lw',
+    x: '47.26%',
+    y: '93.67%',
   },
   'Rock Rupee Cave by Desert': {
     // maybe
     region: 'Desert Area',
     tag: 'lw',
+    x: '31.05%',
+    y: '95.70%',
   },
   'Regular Cave by Desert': {
     // maybe
     region: 'Desert Area',
     tag: 'lw',
+    x: '27.63%',
+    y: '89.14%',
   },
   "Aginah's Cave": {
     region: 'Desert Area',
     tag: 'lw',
+    x: '19.86%',
+    y: '82.58%',
   },
   'Checkerboard Cave': {
     region: 'Desert Area',
     tag: 'lw',
+    x: '17.58%',
+    y: '78.05%',
   },
   'Uncle Drop': {
     region: 'Hyrule Castle',
     tag: 'lw',
     drop: true,
+    x: '60.27%',
+    y: '41.18%',
   },
   'Uncle Door': {
     region: 'Hyrule Castle',
     tag: 'lw',
+    x: '55.48%',
+    y: '42.76%',
   },
 
   // Dark World
@@ -500,42 +636,62 @@ window.doorLocations = {
   'Hyrule Castle Main Entrance': {
     region: 'Hyrule Castle',
     tag: 'lw',
+    x: '50.46%',
+    y: '44.12%',
   },
   'Hyrule Castle Left': {
     region: 'Hyrule Castle',
     tag: 'lw',
+    x: '45.53%',
+    y: '38.46%',
   },
   'Hyrule Castle Right': {
     region: 'Hyrule Castle',
     tag: 'lw',
+    x: '55.48%',
+    y: '38.46%',
   },
   'Hyrule Castle Agahnim': {
     region: 'Hyrule Castle',
     tag: 'lw',
+    x: '50.46%',
+    y: '40.05%',
   },
   'Eastern Palace': {
     region: 'Light World East',
     tag: 'lw',
+    x: '96.58%',
+    y: '38.69%',
   },
   'Desert Palace Main Entrance': {
     region: 'Desert Area',
     tag: 'lw',
+    x: '7.31%',
+    y: '81.22%',
   },
   'Desert Palace West': {
     region: 'Desert Area',
     tag: 'lw',
+    x: '3.20%',
+    y: '79.19%',
   },
   'Desert Palace East': {
     region: 'Desert Area',
     tag: 'lw',
+    x: '11.42%',
+    y: '79.19%',
   },
   'Desert Palace Back': {
     region: 'Desert Area',
     tag: 'lw',
+    x: '7.31%',
+    y: '76.47%',
   },
   'Tower of Hera': {
     region: 'Death Mountain',
     tag: 'lw',
+    x: '56.62%',
+    y: '3.85%',
   },
 
   // Dark world dungeons
